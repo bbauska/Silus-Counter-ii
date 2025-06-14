7d52dc1332c44d781c6844ef9972a86eb9c03148
Brian Bauska <brianbauska@gmail.com>
2025-06-13 20:48:13 -0700

    Add files via upload

---
+++ js/Confetti.js
@@ -0,0 +1,17 @@
+const confettiContainer = document.querySelector('#confetti-container');
+const showConfetti = () => {
+  const confetti = document.createElement('div');
+  confetti.textContent = '☢️';
+  confetti.classList.add('confetti');
+  innerWidth = innerWidth * 0.5;
+  confetti.style.left = Math.random() * innerWidth + 'px';
+  confettiContainer.appendChild(confetti);
+
+  setTimeout(() => {
+    confetti.remove();  /* confetti.remove */
+  }, 3000);  /* wait 3 seconds, was 5000 */
+};
+
+setInterval(() => {
+  showConfetti();
+}, 1000);  /* 400 */
