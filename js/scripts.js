8a4e9d4aa97f5ba548aa6109112e7417d05c93ae
Brian Bauska <brianbauska@gmail.com>
2025-06-13 20:47:01 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
+/* ./js/scripts.js in silus-count-II of silus-counter-ii.bauska.org */
+/* 
+   dd/mm/yy = 0 */
+let counter = 0;
+/* all times are approximate. */
 
+function count() {
+  counter++;
+  givenNumber = counter;
+  output = givenNumber.toLocaleString('en-US'); 
+  document.getElementById('number').innerHTML = output;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('clicker').onclick = count;
+})
