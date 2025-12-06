7071072c1592033237946f3dee7aa2c25bc0aff0
Brian Bauska <brianbauska@gmail.com>
2025-12-06 14:59:59 -0800

    Update Confetti.js

--- js/Confetti.js
+++ js/Confetti.js
@@ -1,4 +1,4 @@
-/* js/Confetti.js of Silus-Counter-3.bauska.org */
+/* ./js/Confetti.js of Silus-Counter-3.bauska.org */
 const confettiContainer = document.querySelector('#confetti-container');
 const showConfetti = () => {
   const confetti = document.createElement('div');
